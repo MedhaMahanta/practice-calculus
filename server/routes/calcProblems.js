daa261c66037b9229954e7c654e01ceeb4197853
Medha Mahanta <medhamahanta@ucsb.edu>
2022-12-31 13:29:23 -0800

    add problems

--- server/routes/calcProblems.js
+++ server/routes/calcProblems.js
@@ -151,6 +151,98 @@ const calcProblems = [{
         topic: "1.3",
         tags: ["Derivatives"],
         solution: "sample solution"
+    }, {
+        //9
+        id: 18,
+        question: "Determine whether ${\\it f(x)}$ approaches ${\\infty}$ or ${-\\infty}$ as ${\\it x}$ approaches ${4}$ from the left, for ${{\\it f(x)} = \\dfrac{1} {({\\it x}-4)^2} }$. Enter 'positive infinity' for ${\\infty}$ or 'negative infinity' for ${-\\infty}$. ",
+        answer: "2x",
+        unit: 1,
+        topic: "1.3",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //19
+        id: 19,
+        question: "Find the equations of the vertical asymptotes of the graph of ${\\it f(x)} = \\dfrac{{\\it x}^2}{ {\\it x}^2-4 }$. (Enter as a comma separated list of equations. For example: x=0, x=1)",
+        answer: "2x",
+        unit: 1,
+        topic: "1.3",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //43
+        id: 20,
+        question: "Find the one-sided limit: $\\lim_{{\\it x} \\to -4^-} {{\\it x}^2 + \\dfrac{2}{ {\\it x}+4 }}$. (Enter 'positive infinity' for ${\\infty}$ and 'negative infinity' for ${-\\infty}$.)",
+        answer: "2x",
+        unit: 1,
+        topic: "1.3",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //45
+        id: 21,
+        question: "Find the one-sided limit: $\\lim_{{\\it x} \\to 0^+} {sin {\\it x} + \\dfrac{1}{ \\it x }}$. (Enter 'positive infinity' for ${\\infty}$ and 'negative infinity' for ${-\\infty}$.)",
+        answer: "2x",
+        unit: 1,
+        topic: "1.3",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //25
+        id: 22,
+        question: "Find the limit: $\\lim_{{\\it x} \\to -{\\infty}} {\\dfrac{\\it x}{ \\sqrt{{\\it x}^2 + {\\it x} }}}$. (Enter 'positive infinity' for ${\\infty}$ and 'negative infinity' for ${-\\infty}$.)",
+        answer: "2x",
+        unit: 1,
+        topic: "1.3",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    },
+    //--------------------------------------------SECTION 1.4 ----------------------------------
+    {
+        //63
+        id: 23,
+        question: "Find the limit of the trigonometric function: $\\lim_{{\\it x} \\to 0} {\\dfrac{sin \\it x}{ 5\\it x}}$. (Enter your answer as a common fraction.)",
+        answer: "1/5",
+        unit: 1,
+        topic: "1.4",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //65
+        id: 24,
+        question: "Find the limit of the trigonometric function: $\\lim_{{\\it x} \\to 0} {\\dfrac{(sin {\\it x})(1 - cos \\it x)}{ \\it x^2}}$. (Enter your answer as a common fraction.)",
+        answer: "1/5",
+        unit: 1,
+        topic: "1.4",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //68
+        id: 25,
+        question: "Find the limit of the trigonometric function: $\\lim_{{\\it x} \\to 0} {\\dfrac{tan^2 \\it x}{ \\it x}}$. (Enter your answer as a common fraction.)",
+        answer: "1/5",
+        unit: 1,
+        topic: "1.4",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //23
+        id: 26,
+        question: "Find the limit: $\\lim_{{\\it x} \\to 1} { \\begin{Bmatrix} {\\it x}^3+1, & {\\it x} < 1 \\\\ {\\it x}+1, & {\\it x} \\geq 1 \\end{Bmatrix} }$.",
+        answer: "1/5",
+        unit: 1,
+        topic: "1.4",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //51
+        id: 27,
+        question: "Find the x-values (if any) at which f is not continuous: ${{\\it f(x)} = \\begin{Bmatrix} 0.5{\\it x}+1, & {\\it x} \\leq 2 \\\\ 3-{\\it x}, & {\\it x} > 2 \\end{Bmatrix} }$.",
+        answer: "1/5",
+        unit: 1,
+        topic: "1.4",
+        tags: ["Derivatives"],
+        solution: "sample solution"
     },
     
     //--------------------------------------------SECTION 2.1 ----------------------------------
