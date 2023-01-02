b124de90cb9061835d58efac008615b62a23f258
Medha Mahanta <medhamahanta@ucsb.edu>
2023-01-01 16:07:34 -0800

    organize files and add unit 2 problems

---
+++ server/routes/problem_lists/unit2Problems.js
@@ -0,0 +1,295 @@
+const unit2Problems = [{
+    //--------------------------------------------SECTION 2.1 ----------------------------------
+        //9
+        id: 28,
+        question: "Find the slope of the tangent line to the graph of the function $ {\\it f(x)} = 3 - 5 {\\it x}$ at the point $(-1, 8)$.",
+        answer: "-5",
+        unit: 2,
+        topic: "2.1",
+        tags: ["Derivatives", "Graphs"],
+        solution: "sample solution"
+    }, {
+        //14
+        id: 29,
+        question: "Find the slope of the tangent line to the graph of the function $ {\\it f(x)} = {\\it x}^2 + 4 {\\it x}$ at the point $(1, 5)$.",
+        answer: "6",
+        unit: 2,
+        topic: "2.1",
+        tags: ["Derivatives", "Graphs"],
+        solution: "sample solution"
+    }, {
+        //21
+        id: 30,
+        question: "Find the derivative of the function $ {\\it f(x)} = {\\it x}^2 + {\\it x} + 3$ by the limit process.",
+        answer: "f'(x) = 2x + 1",
+        unit: 2,
+        topic: "2.1",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //92
+        id: 31,
+        question: "Find the average rate of change of the function $ {\\it f(x)} = {\\it x}^2 -7$ over the interval $[3, 3.1]$. On your own, compare this average rate of change with the instantaneous rates of change at the endpoints of the interval.",
+        answer: "6.1",
+        unit: 2,
+        topic: "2.1",
+        tags: [],
+        solution: "sample solution"
+    }, 
+    //--------------------------------------------SECTION 2.2 ----------------------------------
+    
+    //--------------------------------------------SECTION 2.3 ----------------------------------
+
+    {
+        //29
+        id: 32,
+        question: "Find the equation of the tangent line to the function $ {\\it f(x)} = {\\it x}^2 +3$ at the point $(-1, 4)$.",
+        answer: "y = -2x + 2",
+        unit: 2,
+        topic: "2.3",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //33
+        id: 33,
+        question: "Find the equation of the tangent line to the function $ {\\it f(x)} = \\sqrt{\\it x}$ at the point $(1, 1)$.",
+        answer: "y = x/2 + 1/2",
+        unit: 2,
+        topic: "2.3",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //53
+        id: 34,
+        question: "The tangent line to the graph of $ {\\it y} = {\\it g(x)}$ at the point $(4, 5)$ passes through the point $(7, 0)$. Find ${\\it g'}(4)$.",
+        answer: "-5/3",
+        unit: 2,
+        topic: "2.3",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //5
+        id: 35,
+        question: "Find the tangent line approximation $\\it T$ to the graph of $ {\\it y} = {\\it x}^2$ at the point $(2, 4)$.",
+        answer: "y = 4x - 4",
+        unit: 2,
+        topic: "2.3",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //5
+        id: 36,
+        question: "Use the tangent line approximation to the graph of $ {\\it y} = {\\it x}^2$ at the point $(2, 4)$ to approximate the value of ${\\it f}(2.01)$.",
+        answer: "4.04",
+        unit: 2,
+        topic: "2.3",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //19
+        id: 37,
+        question: "Find the differential $\\it dy$ of the function $ {\\it y} = 3{\\it x}^2-4$.",
+        answer: "6x dx",
+        unit: 2,
+        topic: "2.3",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //43
+        id: 38,
+        question: "Use differentials to approximate the value of the expression $\\sqrt {99.4}$.",
+        answer: "9.97",
+        unit: 2,
+        topic: "2.3",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    },
+    //--------------------------------------------SECTION 2.4 ----------------------------------
+    {
+        //17
+        id: 39,
+        question: "Use the rules of differentiation to find the derivative of the function ${\\it f(x)} = -3{\\it x}^2 + 2 {\\it x} -4$.",
+        answer: "f'(x) = - 6x + 2",
+        unit: 2,
+        topic: "2.4",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //23
+        id: 40,
+        question: "Use the rules of differentiation to find the derivative of the function ${\\it f(x)} = \\dfrac{\\pi}{2} \\sin\\theta$.",
+        answer: "f'(x) = pi/2 cos theta",
+        unit: 2,
+        topic: "2.4",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //25
+        id: 41,
+        question: "Use the rules of differentiation to find the derivative of the function ${\\it f(x)} = {\\it x}^2 - \\dfrac{1}{2} \\cos{\\it x}$.",
+        answer: "f'(x) = 2x + (1/2)sin x",
+        unit: 2,
+        topic: "2.4",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //35
+        id: 42,
+        question: "Find the slope of the graph of the function ${\\it f(x)} = (4{\\it x} + 1)^2$ at the point $(0, 1)$.",
+        answer: "8",
+        unit: 2,
+        topic: "2.4",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //43
+        id: 43,
+        question: "Find the derivative of the function ${\\it f(x)} = \\dfrac{{\\it x}^3 -3{\\it x}^2 + 4}{{\\it x}^2}$.",
+        answer: "f'(x) = (x^3 - 8)/x^3",
+        unit: 2,
+        topic: "2.4",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //51
+        id: 44,
+        question: "Find the derivative of the function ${\\it f(x)} = 6\\sqrt{\\it x} + 5\\cos{\\it x}$.",
+        answer: "3/sqrt(x) - 5 sin x",
+        unit: 2,
+        topic: "2.4",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //111
+        id: 45,
+        question: "Find $\\it a$ and $\\it b$ such that $\\it f$ is differentiable everywhere: ${\\it f(x)} = \\begin{Bmatrix} {\\it ax}^3, & {\\it x} \\leq 2 \\\\ {\\it x}^2+{\\it b}, & {\\it x} > 2 \\end{Bmatrix}$.",
+        answer: "a = 1/3, b = -4/3",
+        unit: 2,
+        topic: "2.4",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    },
+    //--------------------------------------------SECTION 2.5 ----------------------------------
+    {
+        //19
+        id: 46,
+        question: "Find ${\\it f\\>'(c)}$ where ${\\it f(x)}= \\dfrac{{\\it x}^2-4}{{\\it x}-3}$ and ${\\it c} = 1$.",
+        answer: "-1/4",
+        unit: 2,
+        topic: "2.5",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //21
+        id: 47,
+        question: "Find ${\\it f\\>'(c)}$ where ${\\it f(x)}= {\\it x} \\cos {\\it x}$ and ${\\it c} = \\dfrac{\\pi}{4}$.",
+        answer: "(sqrt 2)/2 - (pi sqrt 2)/8",
+        unit: 2,
+        topic: "2.5",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //45
+        id: 48,
+        question: "Find the derivative of the function ${\\it f(x)}= -{\\it x} + \\tan {\\it x}$.",
+        answer: "tan^2 x",
+        unit: 2,
+        topic: "2.5",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //51
+        id: 49,
+        question: "Find the derivative of the function ${\\it f(x)}= -\\csc{\\it x} - \\sin {\\it x}$.",
+        answer: "cos x cot^2 x",
+        unit: 2,
+        topic: "2.5",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //95
+        id: 50,
+        question: "Find the second derivative of the function ${\\it f(x)}= \\dfrac{\\it x}{{\\it x}-1}$.",
+        answer: "2/(x-1)^3",
+        unit: 2,
+        topic: "2.5",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //103
+        id: 51,
+        question: "If ${\\it f\\>''(x)} = -\\sin {\\it x}$, find the higher order derivative ${\\it f\\>}^{(8)}(\\it x)$.",
+        answer: "sin x",
+        unit: 2,
+        topic: "2.5",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    },
+    //--------------------------------------------SECTION 2.6 ----------------------------------
+    {
+        //9
+        id: 52,
+        question: "Find the derivative of the function ${\\it y}= (2{\\it x} -7)^3$.",
+        answer: "y' = 6(2x - 7)^2",
+        unit: 2,
+        topic: "2.6",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //21
+        id: 53,
+        question: "Find the derivative of the function ${\\it y}= \\dfrac{1}{\\sqrt{3{\\it x} +5}}$.",
+        answer: "-3/(2 sqrt (3x + 5)^3)",
+        unit: 2,
+        topic: "2.6",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //33
+        id: 54,
+        question: "Find the derivative of the function ${\\it y}= \\cos 4\\it x $.",
+        answer: "y' = -4 sin 4x",
+        unit: 2,
+        topic: "2.6",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //37
+        id: 55,
+        question: "Find the derivative of the function ${\\it g(x)}= 5\\tan 3{\\it x} $.",
+        answer: "g'(x) = 15 sec^2 3x",
+        unit: 2,
+        topic: "2.6",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //45
+        id: 56,
+        question: "Find the derivative of the function ${\\it y}= 4\\sec^2 {\\it x} $.",
+        answer: "y' = 8 sec^2 x tan x",
+        unit: 2,
+        topic: "2.6",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //63
+        id: 57,
+        question: "Find the slope of the graph of the function ${\\it y}= \\sqrt{{\\it x}^2 +8{\\it x}}$ at the point $(1, 3)$.",
+        answer: "5/3",
+        unit: 2,
+        topic: "2.6",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }, {
+        //89
+        id: 58,
+        question: "Find the second derivative of the function ${\\it y}= \\dfrac{1}{9} (3{\\it x} + 1)^3$ at the point $(1, \\dfrac{64}{9})$.",
+        answer: "24",
+        unit: 2,
+        topic: "2.6",
+        tags: ["Derivatives"],
+        solution: "sample solution"
+    }
+];
+    
+    export default unit2Problems;
